8bba3b870ce3534adc4fb56cb65fd06f9ce8a006
Christian Pena <christianpena9@gmail.com>
2017-09-09 12:17:39 -0400

    Select newly created note. Sort notes by updatedAt

--- imports/ui/NoteListHeader.test.js
+++ imports/ui/NoteListHeader.test.js
@@ -4,19 +4,43 @@ import expect from 'expect';
 import { mount } from 'enzyme';
 
 import { NoteListHeader } from './NoteListHeader';
+import { notes } from '../fixtures/fixtures';
 
 if(Meteor.isClient) {
     
     describe('NoteListHeader', function() {
+        let meteorCall;
+        let Session;
         
-        it('should call meteorcall on click', function() {
-            const spy = expect.createSpy();
-            const wrapper = mount( <NoteListHeader meteorCall={spy} /> );
+        beforeEach(function() {
+            meteorCall = expect.createSpy();
+            Session = {
+                set: expect.createSpy()
+            }
+        });
+        
+        it('should call meteorCall on click', function() {
+            
+            const wrapper = mount( <NoteListHeader meteorCall={meteorCall} Session={Session} /> );
             
             wrapper.find('button').simulate('click');
+            meteorCall.calls[0].arguments[1](undefined, notes[0]._id);
             
-            expect(spy).toHaveBeenCalledWith('notes.insert');
+            expect(meteorCall.calls[0].arguments[0]).toBe('notes.insert');
+            expect(Session.set).toHaveBeenCalledWith('selectedNoteId', notes[0]._id);
         });
+        
+        it('should not set session for failed insert', function() {
+            
+            const wrapper = mount( <NoteListHeader meteorCall={meteorCall} Session={Session} /> );
+            
+            wrapper.find('button').simulate('click');
+            meteorCall.calls[0].arguments[1]({}, undefined); //first parameter is for error, we can pass anything
+            
+            expect(meteorCall.calls[0].arguments[0]).toBe('notes.insert');
+            expect(Session.set).toNotHaveBeenCalled();
+        });
+        
     });
     
 }
