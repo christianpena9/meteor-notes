0e6397598fa009613fd2cfb2e4a8939e3c51b627
Christian Pena <christianpena9@gmail.com>
2017-09-10 15:54:31 -0400

    Setup mobile navigation

--- imports/ui/Dashboard.js
+++ imports/ui/Dashboard.js
@@ -7,7 +7,7 @@ import Editor from './Editor';
 const Dashboard = () => {
     return (
         <div>
-            <PrivateHeader title="Dashbord"/>
+            <PrivateHeader title="Notes"/>
             <div className="page-content">
                 <div className="page-content__sidebar">
                     <NoteList />
